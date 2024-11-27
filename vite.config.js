96b6b8d0664d8cd609252f393c0bf1a9bb3b11e7
VamsiMutyala998 <vamsimuthyala998@gmail.com>
2024-11-27 19:13:07 +0530

    config: add netlify deploy files

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,4 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/urban_loop/',
-  publicPath: process.env.NODE_ENV === 'production' ? '/urban_loop/' : '/',
 })
