9ff044ea4f4708855b577069f83966011875c0b9
grt77 <36742299+grt77@users.noreply.github.com>
2024-12-22 16:16:47 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: true, // Exposes the server to all network interfaces
+  },
 })
