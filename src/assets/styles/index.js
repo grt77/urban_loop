7582808b5c5b66db3cde696e42abd864e99b6136
VamsiMutyala998 <vamsimuthyala998@gmail.com>
2025-01-28 13:03:56 +0530

    UI: Minor css link removed

--- src/assets/styles/index.js
+++ src/assets/styles/index.js
@@ -2,5 +2,4 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import '@fortawesome/fontawesome-free/css/all.css';
 import "@steveyuowo/vue-hot-toast/vue-hot-toast.css";
-import 'vue-step-progress/dist/main.css';
 // import 'https://fonts.googleapis.com/css2?family=Bangers&display=swap';
